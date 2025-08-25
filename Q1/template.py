69f1f8df15825754eb65eb2d07de98541d006415
Balaji <balajikaredla4@gmail.com>
2025-08-26 01:10:10 +0530

    push my coooooooooooooooode

--- Q1/template.py
+++ Q1/template.py
@@ -1,7 +1,5 @@
 import json
 
-
-
 ########################################################################
 
 # Do not install any external packages. You can only use Python's default libraries such as:
@@ -9,8 +7,60 @@ import json
 
 ########################################################################
 
+class JunctionChain:
+    def __init__(self):
+        self.cliques = []
 
+    def add_clique(self, clique):
+        self.cliques.append(clique)
 
+class Clique:
+    def __init__(self, num_nodes, num_states, vars=None):
+        self.num_nodes = num_nodes
+        self.potential = [1,] * (num_states ** num_nodes)
+        self.vars = vars
+        self.num_states = num_states
+        self.index_map = dict()
+        for i in range(len(vars)):
+            self.index_map[vars[i]] = i
+
+    def get_potential(self, assignment):
+        index = 0
+        for i, val in enumerate(assignment):
+            index += val * (self.num_nodes ** (len(assignment) - i - 1))
+        return self.potential[index]
+    
+    def set_potential_from_assignment(self, assignment, value):
+        index = 0
+        for i, val in enumerate(assignment):
+            index += val * (self.num_nodes ** (len(assignment) - i - 1))
+        self.potential[index] = value
+
+    def set_potential_from_index(self, index, value):
+        self.potential[index] = value
+
+    def set_potential(self, potential):
+        self.potential = potential
+
+    def get_index_of_var(self, var):
+        return self.index_map[var]
+
+class Message:
+    def __init__(self, vars, potential):
+        self.vars = vars
+        self.potential = potential
+        self.index_map = dict()
+        for i in range(len(vars)):
+            self.index_map[vars[i]] = i
+
+    def get_potential(self, assignment):
+        index = 0
+        for i, val in enumerate(assignment):
+            index += val * (self.num_nodes ** (len(assignment) - i - 1))
+        return self.potential[index]
+    
+    def get_index_of_var(self, var):
+        return self.index_map[var]
 
 class Inference:
     def __init__(self, data):
@@ -41,6 +91,19 @@ class Inference:
         self.state_factor_potentials_sum = [sum(self.state_factor_potentials[i:i+self.states_count]) for i in range(0, len(self.state_factor_potentials), self.states_count)]
         self.state_factor_potentials = [potential/self.state_factor_potentials_sum[i//self.states_count] for i, potential in enumerate(self.state_factor_potentials)]
         self.k = data['K']
+
+        self.junction_chain = JunctionChain()
+
+        # print('Factors count: ', self.factors_count)
+        # print('States count: ', self.states_count)
+        # print('Number of observations: ', self.num_observations)
+        # print('Observation sequence: ', self.observation_sequence)
+        # print('Transition potentials: ', self.transition_potentials)
+        # print('State factor potentials: ', self.state_factor_potentials)
+        # print('K: ', self.k)
+        # self.triangulate_and_get_cliques()
+        self.get_junction_tree()
+        
         
     def triangulate_and_get_cliques(self):
         """
@@ -54,7 +117,7 @@ class Inference:
 
         Refer to the problem statement for details on triangulation and clique extraction.
         """
-        pass
+        pass        
 
     def get_junction_tree(self):
         """
@@ -68,7 +131,26 @@ class Inference:
 
         Refer to the problem statement for details on junction tree construction.
         """
-        pass
+        print('No of observations: ', self.num_observations)
+        for i in range(self.num_observations):
+            vars = list(range(i*self.factors_count, i*self.factors_count + self.factors_count))
+            clique = Clique(self.factors_count + 1, \
+                            self.states_count, \
+                            vars=list(range(i*self.factors_count, i*self.factors_count + self.factors_count)) + \
+                                [self.num_observations * self.factors_count + i, ])
+            clique.set_potential(self.state_factor_potentials)
+
+            if (i*self.factors_count + self.factors_count) < (self.num_observations * self.factors_count):
+                for j in range(self.factors_count):
+                    child_clique = Clique(self.factors_count + 1, \
+                                          self.states_count, \
+                                            vars=list(range(i*self.factors_count + j, i*self.factors_count + j + self.factors_count + 1)))
+                    
+                    mod = self.states_count ** (self.factors_count - 1)
+                    for i in range(self.states_count * mod):
+                        child_clique.set_potential_from_index(i, \
+                                                              self.transition_potentials[j][(i // mod) * 2 + (i % 2)])
+
 
     def assign_potentials_to_cliques(self):
         """
@@ -94,7 +176,10 @@ class Inference:
         
         Refer to the problem statement for details on computing the partition function.
         """
-        pass
+        first_message = Message([], [1,])
+        for clique in self.junction_chain.cliques:
+            for i in range(self.num_states ** (self.factors_count + 1)):
+                pass
 
     def compute_marginals(self):
         """
